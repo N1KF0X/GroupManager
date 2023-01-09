368cf89658bcdd2a8144d357f17e1839d6c104db
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2023-01-09 18:58:21 +0300

    update

--- GroupManager/urls.py
+++ GroupManager/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from manager import views
+from django.contrib import admin
 from django.views.generic import TemplateView
  
 urlpatterns = [
     path("", TemplateView.as_view(template_name="about.html")),
+    path('admin/', admin.site.urls),
 ]
--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User():
+    login = models.CharFiels(max_length = 20)
+    password = models.CharFiels(max_length = 20)
+
+
+class Group():
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    name = models.CharFiels(max_length = 20)
+    course = models.CharFiels(max_length = 20)
+    minAge = models.PositiveIntegerField()
+    maxAge = models.PositiveIntegerField()
+    m
+
+
