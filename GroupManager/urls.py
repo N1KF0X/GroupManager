fd7027d09601f59e6a041837be42f32d1b4dff0b
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2023-01-09 21:36:35 +0300

    table scrolling

--- GroupManager/urls.py
+++ GroupManager/urls.py
@@ -5,5 +5,7 @@ from django.views.generic import TemplateView
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="main.html")),
+    path("", views.home),
+    path("main", views.main),
+    path("about", views.about),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import*
 
-def start(request):
+def main(request):
+    group = Group.objects.get(id = 1)
+    groupData = []   
+    groupData += [group.name, group.course, "{group.minAge} - {group.maxAge}", "0/{group.capacity}"]
+    data = {"title": "Менеджер групп", "groupData": groupData}
+    return render(request, "main.html", data)
+
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
     return render(request, "about.html")
